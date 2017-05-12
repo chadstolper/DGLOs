c25461966dcbf4d0482a93748f4bf14803e0c3b1
Chad Stolper <chadstolper@gmail.com>
2017-05-12 09:16:13 -0500

    work on dummy graph with ts

--- src/ts/main.ts
+++ src/ts/main.ts
@@ -1,34 +1,64 @@
 // main.ts
-import * as d3 from "d3";
+import * as d3 from "d3-selection";
+import { Selection } from "d3-selection";
+import { scaleOrdinal, scaleLinear, schemeCategory10 } from "d3-scale";
+import { json as d3json } from "d3-request";
 import { Graph, Node, Edge } from "./Graph";
-import { Person, Drink, DrinkEdge, dummyJsonToGraph } from "./dummy_graph";
+import { Person, Drink, DrinkEdge, StaticDrinkGraph } from "./DummyGraph";
 
 
-let colorscale = d3.scaleOrdinal(d3.schemeCategory10);
+let width = 500;
+let height = 500;
 
-let chart = d3.select("body").append("svg")
-	.attr("width", 500)
-	.attr("height", 500);
+let colorscale = scaleOrdinal(schemeCategory10);
+let widthscale = scaleLinear().rangeRound([0, width])
 
-d3.json("dummy.json", function(response){
+let chart: Selection<any, any, any, any> = d3.select("body").append("svg")
+	.attr("width", width)
+	.attr("height", height);
 
-	let rawNodeData:Array<any> = response[0].nodes;
-	let rawEdgeData:Array<any> = response[0].edges;
-	let nodeData:Graph = dummyJsonToGraph(rawNodeData, rawEdgeData);
+d3json("data/dummy/dummy.json", function (response: any) {
 
-	let nodes = chart.selectAll(".node")
-		.data(nodeData.nodes) //calls the getter
-	
+	let graph: Graph = new StaticDrinkGraph(response);
+
+	widthscale.domain([0, graph.nodes.length - 1]);
+
+	let nodes: Selection<any, any, any, any> = chart.selectAll(".node")
+		.data(graph.nodes, function (d: Node): string { return "" + d.id; }) //calls the id getter
+
 	nodes.enter().append("g")
-		.classed("node",true)
-	
-	nodes.style("fill",function(d:Node){
-		//we are letting TS check that d is the right type
-		//and warn us if it is ever not
-		return colorscale(d.type)
-	})
+		.classed("node", true)
+		// let circles = nodes.selectAll("circle")
+		// 		.datum(function(d:Node):Node { return d; })
+		.append("circle")
+			.attr("r", 5)
+			.attr("cx", 0)
+			.attr("cy", 0)
+		
+		console.log(nodes)
 
-	
+	let nodeUpdate: Selection<any,any,any,any> = nodes.enter().merge(nodes)
+	console.log(nodeUpdate)
+	nodeUpdate
+		// .each(function (d: Node, i: number) {
+		// 	console.log(d);
+		// })
+		.style("fill", function (d: Node): string {
+			console.log(d)
+			//we are letting TS check that d is the right type
+			//and warn us if it is ever not
+			console.log(d.type, d.id);
+			console.log(colorscale(d.type));
+			return colorscale(d.type);
+		})
+		.attr("y", 10)
+		.attr("x", function (d: Node, i: number): number {
+			return widthscale(i);
+		});
+
+
+
+
 })
 
 
