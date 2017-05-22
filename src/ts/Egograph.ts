d4ff2646f21f8395debd2c75f0bf1f2d5591a75a
Will Price <willprice22@hotmail.com>
2017-05-22 10:58:05 -0500

    Added some functions to Egograph.ts

--- src/ts/Egograph.ts
+++ src/ts/Egograph.ts
@@ -7,7 +7,34 @@ import { HeatmapTimeline } from "./HeatmapTimeline"
 
 d3json("data/dummy/dummy.json", function (response: any) {
 
+	let graph: DynamicDrinkGraph = new DynamicDrinkGraph(response);
+	let width = 750;
+	let height = 750;
+	let centralNode = null;
+	let incidentEdges = [];
+	let neighborNodes = [];
+	let svg = d3.selectAll("body").append("svg")
+		.attr("width", width)
+		.attr("height", height);
 
+	function initCentralNode(nodeID: number) {
+		for (let i = 0; i < graph.timesteps[0].nodes.length; i++) {
+			if (nodeID === graph.timesteps[0].nodes[i].id) {
+				return graph.timesteps[0].nodes[i];
+			}
+		}
+		return null;
+	}
+
+	function init(centralNodeID: number) {
+		centralNode = initCentralNode(centralNodeID);
+		if (centralNode === null) {
+			init(0);
+		}
+	}
+
+	init(20);
+	console.log(centralNode);
 
 
 });
