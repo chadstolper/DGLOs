b2d62c5cc6297f75466c978526232d57352edd1d
Sanfordm <sanfordm@southwestern.edu>
2017-05-23 15:06:37 -0500

    Interface now exists

---
+++ src/ts/interface.ts
@@ -0,0 +1,20 @@
+import * as d3 from "d3";
+import { Selection } from "d3-selection";
+import { DynamicGraph, Graph } from "./graph";
+
+interface Visualization {
+	readonly dynamicGraph: DynamicGraph;
+	readonly staticGraph?: Graph;
+	selection: Selection<any, {}, any, {}>;
+	width?: number;
+	height?: number;
+
+	draw(graph: Graph): void;
+
+	constructor(graph: DynamicGraph, div: Selection<any, {}, any, {}>): Visualization;
+	//common varibles
+	//a dgraph
+	//a ? sgraph
+	//common functions
+	//constructors, drawers()
+}
