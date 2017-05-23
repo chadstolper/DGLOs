78a0fbbc3920050b838209404fe7bd85223c89a8
Chad Stolper <chadstolper@gmail.com>
2017-05-23 10:29:35 -0500

    tabs instead of spaces

--- src/ts/mainFD.ts
+++ src/ts/mainFD.ts
@@ -6,10 +6,10 @@ import { DynamicDrinkGraph } from "./DummyGraph";
 let height = 500;
 let width = 500
 let svg = d3.select("#chart").append("svg")
-    .attr("width", width)
-    .attr("height", height);
+	.attr("width", width)
+	.attr("height", height);
 
 json("./data/dummy/dummy.json", function (response) {
-    let dGraph: DynamicDrinkGraph = new DynamicDrinkGraph(response);
-    let FDGraph: ForceDirectedGraph = new ForceDirectedGraph(dGraph, svg, width, height);
+	let dGraph: DynamicDrinkGraph = new DynamicDrinkGraph(response);
+	let FDGraph: ForceDirectedGraph = new ForceDirectedGraph(dGraph, svg, width, height);
 });
