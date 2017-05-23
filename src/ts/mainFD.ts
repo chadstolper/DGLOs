f095021a538927e74956ca986597e60e33bc8188
Chad Stolper <chadstolper@gmail.com>
2017-05-23 10:07:29 -0500

    renamed classes

--- src/ts/mainFD.ts
+++ src/ts/mainFD.ts
@@ -1,5 +1,5 @@
 import * as d3 from "d3";
-import { ForceDirectedGraph } from "./forceDirectedGraph";
+import { ForceDirectedGraph } from "./ForceDirectedGraph";
 import { json } from "d3-request";
 import { DynamicDrinkGraph } from "./DummyGraph";
 
@@ -9,7 +9,7 @@ let svg = d3.select("#chart").append("svg")
     .attr("width", width)
     .attr("height", height);
 
-json("./data/dummy/dummy.json", function(response) {
+json("./data/dummy/dummy.json", function (response) {
     let dGraph: DynamicDrinkGraph = new DynamicDrinkGraph(response);
     let FDGraph: ForceDirectedGraph = new ForceDirectedGraph(dGraph, svg, width, height);
 });
