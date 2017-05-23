68fc1d3d67d0b2982a58bab50f5064886b2cdb92
Chad Stolper <chadstolper@gmail.com>
2017-05-23 13:48:17 -0500

    Testing pure FDGraph (vs. AnimatedFD)

--- src/ts/mainFD.ts
+++ src/ts/mainFD.ts
@@ -1,5 +1,6 @@
 import * as d3 from "d3";
 import { AnimatedForceDirectedGraph } from "./AnimatedForceDirectedGraph";
+// import { ForceDirectedGraph } from "./ForceDirectedGraph";
 import { json } from "d3-request";
 import { DynamicDrinkGraph } from "./DummyGraph";
 import { DynamicGraph } from "./Graph";
@@ -13,6 +14,7 @@ let svg = d3.select("#chart").append("svg")
 json("./data/dummy/dummy.json", function (response) {
 	let dGraph: DynamicGraph = new DynamicDrinkGraph(response);
 	let FDGraph: AnimatedForceDirectedGraph = new AnimatedForceDirectedGraph(dGraph, svg, width, height);
+	// let FDGraph: ForceDirectedGraph = new ForceDirectedGraph(dGraph, svg, width, height);
 	FDGraph.draw(dGraph.timesteps[0]);
 
 });
