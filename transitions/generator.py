a6a0c3b168bdc4306643dc36105d9698389403c1
Chad Stolper <chadstolper@gmail.com>
2017-05-26 15:43:45 -0500

    transitions files (2nd attempt)

---
+++ transitions/generator.py
@@ -0,0 +1,6 @@
+techs = ["fda","fdt","gestalt","gmap","matrixa","matrixt","shi"]
+
+for tech1 in techs:
+	for tech2 in techs:
+		with open(tech1+"::"+tech2+".txt","w") as file:
+			file.write("todo")
