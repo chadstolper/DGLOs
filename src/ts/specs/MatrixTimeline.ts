61056a8a5851e7d33e529aa72e9fabba27fe348e
Chad Stolper <chadstolper@gmail.com>
2017-05-31 16:44:42 -0500

    matix timeline

--- src/ts/specs/MatrixTimeline.ts
+++ src/ts/specs/MatrixTimeline.ts
@@ -3,13 +3,13 @@ import { Technique } from "./Technique"
 export class MatrixTimeline extends Technique {
 	public draw(): void {
 		this.lib.drawNodeGlyphs();
-		this.lib.transformNodeGlyphsTo(/*"label"*/);
-		this.lib.positionNodeGlyphsMatrix(/*x-axis*/);
+		this.lib.transformNodeGlyphsTo(this.lib.labelShape);
+		this.lib.positionNodeGlyphsMatrix();
 		this.lib.drawEdgeGlyphs();
 		this.lib.transformEdgeGlyphsTo(this.lib.rectShape);
 		this.lib.positionEdgeGlyphsMatrix();
-		this.lib.setNodeGlyphAttrs();
-		this.lib.setEdgeGlyphAttrs();
+		this.lib.setNodeGlyphAttrs(this.opts);
+		this.lib.setEdgeGlyphAttrs(this.opts);
 		this.lib.replicateTimesteps();
 	}
 
