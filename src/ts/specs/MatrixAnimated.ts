d5b10b2b1ebc6ad022b5e81f5b88002033802038
Will Price <willprice22@hotmail.com>
2017-05-31 16:49:15 -0500

    Updated parameters in ForceDirectedTimeline.ts and MatrixAnimated.ts

--- src/ts/specs/MatrixAnimated.ts
+++ src/ts/specs/MatrixAnimated.ts
@@ -2,13 +2,13 @@ import { Technique } from "./Technique"
 export class MatrixAnimated extends Technique {
 	public draw(): void {
 		this.lib.drawNodeGlyphs();
-		this.lib.transformNodeGlyphsTo(/*"label"*/);
-		this.lib.positionNodeGlyphsMatrix(/*x-axis*/);
+		this.lib.transformNodeGlyphsTo(this.lib.labelShape);
+		this.lib.positionNodeGlyphsMatrix();
 		this.lib.drawEdgeGlyphs();
-		this.lib.transformEdgeGlyphsTo(/*rect*/);
+		this.lib.transformEdgeGlyphsTo(this.lib.rectShape);
 		this.lib.positionEdgeGlyphsMatrix();
-		this.lib.setNodeGlyphAttrs();
-		this.lib.setEdgeGlyphAttrs();
+		this.lib.setNodeGlyphAttrs(this.opts);
+		this.lib.setEdgeGlyphAttrs(this.opts);
 		this.lib.enableStepping();
 	}
 
