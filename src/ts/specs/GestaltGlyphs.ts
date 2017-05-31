22420247ac86d874d1a89d699072792be1e3d7a0
Chad Stolper <chadstolper@gmail.com>
2017-05-31 16:15:38 -0500

    gestalt glyphs filename change

--- src/ts/specs/GestaltGlyphs.ts
+++ src/ts/specs/GestaltGlyphs.ts
@@ -1,11 +1,11 @@
 import { Technique } from "./Technique";
 
-export class GestaltGlyph extends Technique {
+export class GestaltGlyphs extends Technique {
 	public draw() {
 		this.lib.stopSimulation();
 		this.lib.drawNodeGlyphs();
 		this.lib.drawEdgeGlyphs();
-		this.lib.transformNodeGlyphsTo("label");
+		this.lib.transformNodeGlyphsTo(/*"label"*/);
 		this.lib.positionEdgeGlyphsGestalt();
 		this.lib.positionNodeGlyphsGestalt();
 		this.lib.setNodeGlyphAttrs();
