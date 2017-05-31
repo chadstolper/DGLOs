6eef0cf203cb8822750618e71f68cb8cf04b4917
Chad Stolper <chadstolper@gmail.com>
2017-05-31 15:12:45 -0500

    Proper selection

--- src/ts/specs/Technique.ts
+++ src/ts/specs/Technique.ts
@@ -1,8 +1,9 @@
 import { DynamicGraph } from "../model/DynamicGraph";
+import { Selection } from "d3-selection";
 
 export interface Technique {
 	data(data: DynamicGraph): Technique;
-	location(loc: Selection): Technique;
+	location(loc: Selection<any, {}, any, {}>): Technique;
 	options(args: any): Technique;
 	draw(): Technique;
 }
