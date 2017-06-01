5c7d6804b7dc951657dccabad3dc1788515e6a6f
Sanfordm <sanfordm@southwestern.edu>
2017-06-01 11:26:24 -0500

    DrawNodes Progress..

--- src/ts/lib/DGLOsSVG.ts
+++ src/ts/lib/DGLOsSVG.ts
@@ -8,7 +8,7 @@ export class DGLOsSVG extends DGLOsSVGBaseClass {
 
 	_nodeG: Selection<any, {}, any, {}>;
 	_nodeGlyphs: Selection<any, {}, any, {}>;
-	_timeStamp: number;
+	_timeStamp = this._data.timesteps[0].timestamp;
 
 	public drawNodeGlyphs() {
 		this._nodeG = this.loc.append("g")
@@ -23,6 +23,13 @@ export class DGLOsSVG extends DGLOsSVGBaseClass {
 			.attr("id", function (d: any): string | number { return d.name; });
 
 		this._nodeGlyphs = this._nodeGlyphs.merge(nodeEnter);
+
+		//here for debugging
+		this._nodeGlyphs
+			.attr("fill", "purple")
+			.attr("stroke", "grey")
+			.attr("stroke-width", 3)
+			.attr("r", 25);
 	}
 
 	public drawEdgeGlyphs() {
