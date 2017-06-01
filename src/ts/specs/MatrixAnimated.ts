2dcc4fd81aabedc056d34d2b825f63b0f631c17b
Will Price <willprice22@hotmail.com>
2017-06-01 14:55:40 -0500

    Synch

--- src/ts/specs/MatrixAnimated.ts
+++ src/ts/specs/MatrixAnimated.ts
@@ -2,14 +2,14 @@ import { Technique } from "./Technique"
 export class MatrixAnimated extends Technique {
 	public draw(): void {
 		this.lib.drawNodeGlyphs();
-		this.lib.transformNodeGlyphsTo(this.lib.labelShape);
-		this.lib.positionNodeGlyphsMatrix();
+		//this.lib.transformNodeGlyphsTo(this.lib.labelShape);
+		//this.lib.positionNodeGlyphsMatrix();
 		this.lib.drawEdgeGlyphs();
 		this.lib.transformEdgeGlyphsTo(this.lib.rectShape);
-		this.lib.positionEdgeGlyphsMatrix();
-		this.lib.setNodeGlyphAttrs(this.opts);
-		this.lib.setEdgeGlyphAttrs(this.opts);
-		this.lib.enableStepping();
+		//this.lib.positionEdgeGlyphsMatrix();
+		//this.lib.setNodeGlyphAttrs(this.opts);
+		//this.lib.setEdgeGlyphAttrs(this.opts);
+		//this.lib.enableStepping();
 	}
 
 }
