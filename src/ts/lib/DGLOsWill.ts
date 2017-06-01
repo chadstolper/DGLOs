507b44acd638e70869017bb5d2135a799e6bb9c8
Will Price <willprice22@hotmail.com>
2017-06-01 14:12:37 -0500

    Close to finishing transformEdgeGlyphsToRect()

--- src/ts/lib/DGLOsWill.ts
+++ src/ts/lib/DGLOsWill.ts
@@ -5,6 +5,9 @@ import { DGLOsSVGCombined } from "./DGLOsSVGCombined";
 import { DGLOsMatt } from "./DGLOsMatt";
 import { NodeGlyphShape, EdgeGlyphShape } from "./DGLOs";
 import * as shapes from "./ShapeClasses";
+import * as d3 from "d3-selection";
+import * as d3Scale from "d3-scale";
+import * as d3Array from "d3-array";
 
 export class DGLOsWill extends DGLOsMatt {
 	public drawEdgeGlyphs() {
@@ -21,19 +24,69 @@ export class DGLOsWill extends DGLOsMatt {
 			.attr("x1", 0)
 			.attr("x2", 1)
 			.attr("y1", 0)
-			.attr("y2", 1)
-			.attr("Stroke", "white");
+			.attr("y2", 1);
 
 		this._edgeGlyphs = this._edgeGlyphs.merge(edgeEnter);
 	}
 
-	public transformEdgeGlyphsTo(shape: NodeGlyphShape) {
-		console.log(Object.prototype.toString.call(shape));
+	public transformEdgeGlyphsTo(shape: any) {
+		//if (shape instanceof shapes.RectGlyphShape) {
 		this.transformEdgeGlyphsToRect();
+		//}
+		//console.log("you dummy");
 	}
 	private transformEdgeGlyphsToRect() {
-		console.log("hey");
+		console.log("its a rect");
+		let curGraph = this.data.timesteps[super._timeStampIndex];
+
+
+		let arraySize = curGraph.nodes.length;
+
+		// let colorMap = d3Scale.scaleLinear<string>()
+		// 	.domain(this.createColorDomain(curGraph.edges))
+		// 	.range(this._colorDomain);
+
+		this._edgeG = this.loc.append("g")
+			.classed("edges", true);
+
+		this._edgeGlyphs = this._edgeG.selectAll("line")
+			.data(this.data.timesteps[this._timeStampIndex].edges, function (d: Edge): string { return d.source + ":" + d.target });
+
+		this._edgeGlyphs.exit().remove();
+
+		let cells = super.loc.selectAll("rect")
+			.data(curGraph.edges, function (d: Edge): string { return "" + d.id; })
+
+		cells.exit().remove();
+
+		let cellsEnter = cells.enter()
+			.append("rect")
+			.attr("stroke", "black")
+			.attr("id", function (d: Edge) { return d.id });
+
+		cells = cells.merge(cellsEnter)
+		cells.transition()
+			.attr("x", function (d: Edge) {
+				return (+d.source.id / curGraph.nodes.length) * 100 + "%";
+			})
+			.attr("y", function (d: Edge) {
+				return (+d.target.id / curGraph.nodes.length) * 100 + "%";
+			})
+			.attr("width", this._width / arraySize)
+			.attr("height", this._height / arraySize)
+			.attr("fill", "red");
+
+		// .attr("fill", function (d) {
+		// 	return colorMap(d.weight);
+		// })
 	}
+
+	private createColorDomain(edges: Array<Edge>) {
+		return d3Array.extent(edges, function (d: Edge): number {
+			return d.weight;
+		});
+	}
+
 
 
 }
