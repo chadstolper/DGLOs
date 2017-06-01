d4047446c8a5755d1d99d0db68ce62d792edba8e
Will Price <willprice22@hotmail.com>
2017-06-01 11:33:05 -0500

    Continued work on drawEdgeGlyphs()

--- src/ts/lib/DGLOsSVG.ts
+++ src/ts/lib/DGLOsSVG.ts
@@ -32,7 +32,20 @@ export class DGLOsSVG extends DGLOsSVGBaseClass {
 			.classed("edges", true);
 
 		this._nodeGlyphs = this._edgeG.selectAll("line")
-			.data(this.data.timesteps[this._timeStamp].edges, function (d: Edge): string { return d.source + ":" + d.target });
+			.data(this._data.timesteps[this._timeStamp].edges, function (d: Edge): string { return d.source + ":" + d.target });
+
+		this._edgeGlyphs.exit().remove();
+
+		let edgeEnter = this._edgeGlyphs.enter().append("line")
+			.attr("id", function (d: Edge): string { return d.source + ":" + d.target })
+			.attr("x1", 0)
+			.attr("x2", 1)
+			.attr("y1", 0)
+			.attr("y2", 1)
+			.attr("Stroke", "white");
+
+		this._edgeGlyphs = this._edgeGlyphs.merge(edgeEnter);
 	}
+
 
 }
