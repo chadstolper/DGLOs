1f168e38ec368b1af89ddf4a6edfca6d35e230fd
Sanfordm <sanfordm@southwestern.edu>
2017-06-01 11:48:22 -0500

    commented out things, added timestamp, runSimulation started

--- src/ts/lib/DGLOsSVG.ts
+++ src/ts/lib/DGLOsSVG.ts
@@ -1,20 +1,22 @@
 import { DGLOsSVGBaseClass } from "./DGLOsSVGBaseClass";
 import { Selection } from "d3-selection";
 import { Node, Edge } from "../model/dynamicgraph";
-
+import { ScaleOrdinal, scaleOrdinal, schemeCategory20b } from "d3-scale";
+import * as d3force from "d3-force";
+import { Simulation } from "d3-force";
 
 
 export class DGLOsSVG extends DGLOsSVGBaseClass {
 
 	_nodeG: Selection<any, {}, any, {}>;
 	_nodeGlyphs: Selection<any, {}, any, {}>;
-<<<<<<< HEAD
-	_timeStamp = this._data.timesteps[0].timestamp;
-=======
+	_timeStampIndex = 0;
+	_colorScheme: ScaleOrdinal<string | number, string> = scaleOrdinal<string | number, string>(schemeCategory20b);
 	_edgeG: Selection<any, {}, any, {}>
 	_edgeGlyphs: Selection<any, {}, any, {}>;
-	_timeStamp: number;
->>>>>>> 31cf744f1df286fac0f53d223198eb0e300de768
+	_simulation: Simulation<any, undefined>
+	_height = 500;
+	_width = 500;
 
 	public drawNodeGlyphs() {
 		this._nodeG = this.loc.append("g")
@@ -44,6 +46,35 @@ export class DGLOsSVG extends DGLOsSVGBaseClass {
 
 		this._nodeGlyphs = this._edgeG.selectAll("line")
 			.data(this.data.timesteps[this._timeStamp].edges, function (d: Edge): string { return d.source + ":" + d.target });
+
 	}
 
+	public runSimulation() {
+		this._simulation = d3force.forceSimulation() //init sim for chart?
+			.force("link", d3force.forceLink().id(function (d: Node): string { return "" + d.id })) //pull applied to link lengths
+			.force("charge", d3force.forceManyBody().strength(-50)) //push applied to all things from center
+			.force("center", d3force.forceCenter(this._width / 2, this._height / 2)) //define center
+			.on("tick", this.tick);
+	}
+
+	private tick() {
+		if (this._edgeGlyphs !== undefined) {
+			this._edgeGlyphs //as in the lines representing links
+				.attr("x1", function (d: Edge) { return d.source.x; })
+				.attr("y1", function (d: Edge) { return d.source.y; })
+				.attr("x2", function (d: Edge) { return d.target.x; })
+				.attr("y2", function (d: Edge) { return d.target.y; });
+		} else {
+			console.log("No links!");
+		}
+		if (this._nodeGlyphs !== undefined) {
+			this._nodeGlyphs
+				.attr("cx", function (d: Node) {
+					return d.x;
+				})
+				.attr("cy", function (d: Node) { return d.y; });
+		} else {
+			console.log("No nodes!");
+		}
+	}
 }
--- src/ts/specs/ForceDirectedAnimated.ts
+++ src/ts/specs/ForceDirectedAnimated.ts
@@ -4,10 +4,10 @@ export class ForceDirectedAnimated extends Technique {
 	public draw() {
 		this.lib.drawNodeGlyphs();
 		this.lib.drawEdgeGlyphs();
-		this.lib.transformNodeGlyphsTo(this.lib.circleShape);
-		this.lib.transformEdgeGlyphsTo(this.lib.sourceTargetLineShape);
-		this.lib.setNodeGlyphAttrs(this.opts);
-		this.lib.setEdgeGlyphAttrs(this.opts);
+		// this.lib.transformNodeGlyphsTo(this.lib.circleShape);
+		// this.lib.transformEdgeGlyphsTo(this.lib.sourceTargetLineShape);
+		// this.lib.setNodeGlyphAttrs(this.opts);
+		// this.lib.setEdgeGlyphAttrs(this.opts);
 		this.lib.runSimulation();
 	}
 }
