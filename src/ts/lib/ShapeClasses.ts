2414f377c8aefb98cf032896d51c8bbddac939a6
Will Price <willprice22@hotmail.com>
2017-06-01 13:28:52 -0500

    Added width, height, and fill properties, a constructor, and getters and setters to RectGlyphShape

--- src/ts/lib/ShapeClasses.ts
+++ src/ts/lib/ShapeClasses.ts
@@ -1,8 +1,37 @@
 import { NodeGlyphShape, EdgeGlyphShape } from "./DGLOs";
 
-export class LabelGlyphShape implements NodeGlyphShape { }
+export class LabelGlyphShape implements NodeGlyphShape {
+}
 export class CircleGlyphShape implements NodeGlyphShape { }
 
-export class RectGlyphShape implements EdgeGlyphShape { }
+export class RectGlyphShape implements EdgeGlyphShape {
+	private _width: number;
+	private _height: number;
+	private _fill: string;
+
+	constructor(width: number, height: number, fill: string) {
+		this._width = width;
+		this._height = height;
+		this._fill = fill;
+	}
+	get width(): number {
+		return this._width;
+	}
+	set width(width: number) {
+		this._width = width;
+	}
+	get height(): number {
+		return this._height;
+	}
+	set height(height: number) {
+		this._height = height;
+	}
+	get fill(): string {
+		return this._fill;
+	}
+	set fill(fill: string) {
+		this._fill = fill;
+	}
+}
 export class SourceTargetLineGlyphShape implements EdgeGlyphShape { }
 export class GestaltGlyphShape implements EdgeGlyphShape { }
