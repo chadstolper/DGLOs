9f23d1384b833cd486e3c39f60e79a8d37baeabb
Will Price <willprice22@hotmail.com>
2017-06-02 09:35:25 -0500

    implemented transformEdgeGlyphsTo() switch case, added functions
    transformGestaltToLines()
    
    transformGestaltToRect()
    
    transformLinesToGestalt()
    
    transformRectToGestalt()
    
    (they are unimplemented)

--- src/ts/lib/DGLOsWill.ts
+++ src/ts/lib/DGLOsWill.ts
@@ -31,18 +31,55 @@ export class DGLOsWill extends DGLOsMatt {
 	}
 
 	public transformEdgeGlyphsTo(shape: any) {
-		if (shape.shapeType === "Rect") {
-			this.transformLinesToRect();
+		switch (shape) {
+			case this._currentEdgeShape.shapeType === "Rect":
+				switch (shape) {
+					case shape.shapeType === "STLine":
+						this.transformRectToLines();
+					case shape.shapeType === "Gestalt":
+						this.transformRectToGestalt();
+					default:
+						console.log("Your shape has not been implemented");
+				}
+			case this._currentEdgeShape.shapeType === "STLine":
+				switch (shape) {
+					case shape.shapteType === "Rect":
+						this.transformLinesToRect();
+					case shape.shapeType === "Gestalt":
+						this.transformLinesToGestalt();
+					default:
+						console.log("Your shape has not been implemented");
+				}
+			case this._currentEdgeShape.shapeType === "Gestalt":
+				switch (shape) {
+					case shape.shapteType === "Rect":
+						this.transformGestaltToRect();
+					case shape.shapteType === "STLine":
+						this.transformGestaltToLines();
+					default:
+						console.log("Your shape has not been implemented");
+
+				}
 		}
 	}
 
+	private transformGestaltToLines() {
+		console.log("transfromGestaltToLines not yet implemented :)");
+	}
+	private transformGestaltToRect() {
+		console.log("transfromGestaltToRect not yet implemented :)");
+	}
+	private transformLinesToGestalt() {
+		console.log("transfromLinesToGestalt not yet implemented :)");
+	}
+
+	private transformRectToGestalt() {
+		console.log("transfromRectToGestalt not yet implemented :)");
+	}
 	private transformLinesToRect() {
 		console.log("hello, we're here");
 		let curGraph = this.data.timesteps[this._timeStampIndex];
 		let arraySize = curGraph.nodes.length;
-
-		// this._edgeG = this.loc.append("g")
-		// 	.classed("rectEdges", true);
 
 		// this._edgeLineGlyphs.style("display","hidden");
 		// this._edgeRectGlyphs.style("display",null);
@@ -54,11 +91,6 @@ export class DGLOsWill extends DGLOsMatt {
 
 		let cellsEnter = this._edgeGlyphs.enter()
 			.append("rect")
-		// .attr("stroke", "black")
-		// .attr("width", 0)
-		// .attr("height", 0)
-		// .attr("id", function (d: Edge) { return d.id });
-
 		this._edgeGlyphs = this._edgeGlyphs.merge(cellsEnter)
 	}
 	public positionEdgeGlyphsMatrix() {
@@ -71,7 +103,6 @@ export class DGLOsWill extends DGLOsMatt {
 				return (+d.target.id / curGraph.nodes.length) * 100 + "%";
 			})
 	}
-
 	public transformRectToLines() {
 		this._edgeGlyphs = this._edgeG.selectAll("line")
 			.data(this.data.timesteps[this._timeStampIndex].edges, function (d: Edge): string { return d.source + ":" + d.target });
