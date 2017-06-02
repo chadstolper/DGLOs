43653e11bb05289916d6f112780f687ecaaefea1
Sanfordm <sanfordm@southwestern.edu>
2017-06-02 10:32:09 -0500

    some shape changes

--- src/ts/lib/ShapeClasses.ts
+++ src/ts/lib/ShapeClasses.ts
@@ -9,21 +9,6 @@ export class LabelGlyphShape implements NodeGlyphShape {
 	// private _font = "Arial";
 	// private _font_weight = "Normal";
 	// private _float = "center";
-	private _xPos = 0;
-	private _yPos = 0;
-
-	get xPos(): number {
-		return this._xPos;
-	}
-	set xPos(x: number) {
-		this._xPos = x;
-	}
-	get yPos(): number {
-		return this._yPos;
-	}
-	set yPos(y: number) {
-		this._yPos = y;
-	}
 
 	constructor(text: string, fill: string, x: number, y: number) {
 		this._text = text;
@@ -46,6 +31,22 @@ export class LabelGlyphShape implements NodeGlyphShape {
 	}
 	set fill(fill: string) {
 		this._fill = fill;
+	}
+
+
+	get X(): number {
+		return this._X;
+	}
+	set X(x: number) {
+		this._X = x;
+	}
+
+
+	get Y(): number {
+		return this._Y;
+	}
+	set Y(y: number) {
+		this._Y = y;
 	}
 
 
