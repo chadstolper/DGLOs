32954122a9987dc1f6e8ce8a9113dda98ac74fc7
Sanfordm <sanfordm@southwestern.edu>
2017-06-02 15:39:23 -0500

    partly implementing nodeglyphshpe to labelglyphshape

--- src/ts/lib/NodeGlyphShape.ts
+++ src/ts/lib/NodeGlyphShape.ts
@@ -7,9 +7,11 @@ export interface NodeGlyphShape {
 	init(location: Selection<any, {}, any, {}>): void;
 	//TODO: Make new <g>
 	initDraw(location: Selection<any, {}, any, {}>): Selection<any, {}, any, {}>;
-	//
+	//TODO: draw nodes
 	updateDraw(location: Selection<any, {}, any, {}>): Selection<any, {}, any, {}>;
+	//TODO: position and add attr
 	transformTo(shape: NodeGlyphShape): NodeGlyphShape;
+	//TODO: says what it does on the tin
 	draw(location: Selection<any, {}, any, {}>, data: DynamicGraph, timeStepIndex: number): void;
 	//TODO: .data(data.timestep[timestepindex]).enter().call(initDraw(location))
 }
