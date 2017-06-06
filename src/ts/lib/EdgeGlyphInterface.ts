7f66e5c6d496c6976e5a0bc1dffb83a2cadae7c4
Will Price <willprice22@hotmail.com>
2017-06-06 10:47:40 -0500

    Updated transfromTo() in EdgeGlyphInterface.ts to return void

--- src/ts/lib/EdgeGlyphInterface.ts
+++ src/ts/lib/EdgeGlyphInterface.ts
@@ -5,6 +5,6 @@ export interface EdgeGlyphShape {
 	init(location: Selection<any, {}, any, {}>): Selection<any, {}, any, {}>;
 	initDraw(selection: Selection<any, {}, any, {}>): Selection<any, {}, any, {}>;
 	updateDraw(selection: Selection<any, {}, any, {}>): Selection<any, {}, any, {}>;
-	transformTo(shape: EdgeGlyphShape): EdgeGlyphShape;
+	transformTo(shape: EdgeGlyphShape): void;
 	draw(selection: Selection<any, {}, any, {}>, dGraph: DynamicGraph, TimeStampIndex: number): void;
 }
