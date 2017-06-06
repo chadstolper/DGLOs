80a16af8e3855249553f4f4f6f128f31e621341c
Will Price <willprice22@hotmail.com>
2017-06-06 13:14:13 -0500

    initDraw() and updateDraw() now take an AttrOpts as a parameter

--- src/ts/lib/EdgeGlyphInterface.ts
+++ src/ts/lib/EdgeGlyphInterface.ts
@@ -1,10 +1,11 @@
 import { Selection } from "d3-selection";
 import { DynamicGraph } from "../model/DynamicGraph"
+import { AttrOpts } from "./DGLOs";
 export interface EdgeGlyphShape {
 	readonly shapeType: string;
 	init(location: Selection<any, {}, any, {}>): Selection<any, {}, any, {}>;
-	initDraw(selection: Selection<any, {}, any, {}>): Selection<any, {}, any, {}>;
-	updateDraw(selection: Selection<any, {}, any, {}>): Selection<any, {}, any, {}>;
+	initDraw(selection: Selection<any, {}, any, {}>, attr: AttrOpts): Selection<any, {}, any, {}>;
+	updateDraw(selection: Selection<any, {}, any, {}>, attr: AttrOpts): Selection<any, {}, any, {}>;
 	transformTo(sourceG: Selection<any, {}, any, {}>, targetShape: EdgeGlyphShape, targetG: Selection<any, {}, any, {}>): void;
 	draw(selection: Selection<any, {}, any, {}>, dGraph: DynamicGraph, TimeStampIndex: number): void;
 }
