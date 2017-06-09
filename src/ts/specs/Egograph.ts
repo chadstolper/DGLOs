1c59cefa8771fab618d9e720a7dca9b27e2932bf
Will Price <willprice22@hotmail.com>
2017-06-09 13:33:32 -0500

    Re -added a line that I accidentally deleted previously

--- src/ts/specs/Egograph.ts
+++ src/ts/specs/Egograph.ts
@@ -5,6 +5,7 @@ export class Egograph extends Technique {
 	public draw() {
 		let attr = new SVGAttrOpts("id", "black", 10, 1);
 		this.lib.setCenterNode(this.lib.data.timesteps[0].nodes[0].origID as number);
+		this.lib.fixCentralNodePositions(true);
 		this.lib.drawNodeGlyphs();
 		this.lib.drawEdgeGlyphs();
 		this.lib.transformNodeGlyphsTo(this.lib.circleShape);
