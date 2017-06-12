057a954ca2f3e84dabbea6823d557c86dacf4560
Chad Stolper <chadstolper@gmail.com>
2017-06-12 12:32:04 -0500

    Fixed newcomb reader to include all timesteps instead of the first one 15 times

--- data/newcomb/newcomb_to_json.py
+++ data/newcomb/newcomb_to_json.py
@@ -3,30 +3,30 @@ import csv
 
 timesteps = []
 
-for timestamp in xrange(1,16):
-	nodes = []
-	edges = []
-	filename = 'newfrat'+format(1,'02')+".csv"
-	with open(filename,'r') as datafile:
-		reader = csv.reader(datafile, delimiter="\t")
-		for n in xrange(0,17):
-			nodes.append(n)
+for timestamp in xrange(1, 16):
+    nodes = []
+    edges = []
+    filename = 'newfrat' + format(timestamp, '02') + ".csv"
+    with open(filename, 'r') as datafile:
+        reader = csv.reader(datafile, delimiter="\t")
+        for n in xrange(0, 17):
+            nodes.append(n)
 
-		fr = 0
-		for row in reader:
-			to = 0
-			for val in row:
-				edge = {}
-				edge["ranker"] = fr
-				edge["rankee"] = to
-				edge["rank"] = int(val)
-				edges.append(edge)
-				to+=1
-			fr+=1
-	timestep = {}
-	timestep["timestamp"] = timestamp
-	timestep["nodes"] = nodes
-	timestep["edges"] = edges
-	timesteps.append(timestep)
+        fr = 0
+        for row in reader:
+            to = 0
+            for val in row:
+                edge = {}
+                edge["ranker"] = fr
+                edge["rankee"] = to
+                edge["rank"] = int(val)
+                edges.append(edge)
+                to += 1
+            fr += 1
+    timestep = {}
+    timestep["timestamp"] = timestamp
+    timestep["nodes"] = nodes
+    timestep["edges"] = edges
+    timesteps.append(timestep)
 
 print json.dumps(timesteps, indent=2)
