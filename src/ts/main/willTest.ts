cde4834494d40cee208f4d210507a71491cd486e
Will Price <willprice22@hotmail.com>
2017-06-12 13:23:58 -0500

    willTest is now testing Gestalt, Shape.ts now calls redraw() at the end of transformTo()

--- src/ts/main/willTest.ts
+++ src/ts/main/willTest.ts
@@ -9,6 +9,7 @@ import { select, Selection } from "d3-selection";
 import { json } from "d3-request";
 import { DGLOs } from "../lib/DGLOs";
 import { DGLOsSVG } from "../lib/DGLOsSVG";
+import { GestaltGlyphs } from "../specs/GestaltGlyphs"
 
 
 json("data/radoslaw/emails.json", function (response: any) {
@@ -18,7 +19,7 @@ json("data/radoslaw/emails.json", function (response: any) {
 	let svg: Selection<any, {}, any, {}> = select("body").append("svg")
 		.attr("width", width).attr("height", height);
 	let lib: DGLOs = new DGLOsSVG(g, svg);
-	let vis: Technique = new Egograph(lib, {});
+	let vis: Technique = new GestaltGlyphs(lib, {});
 	vis.draw();
 })
 
