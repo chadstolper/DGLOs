58c668753b9b1dab987634587126905c1575baf5
Will Price <willprice22@hotmail.com>
2017-06-13 16:56:03 -0500

    Fixed EdgeShape and FlubberEdgeShape

--- src/ts/lib/shapes/FlubberEdgeShape.ts
+++ src/ts/lib/shapes/FlubberEdgeShape.ts
@@ -1,14 +1,13 @@
 import { EdgeShape } from "./EdgeShape"
+import { DGLOsSVGBaseClass } from "../DGLOsSVGBaseClass"
+import { Selection } from "d3-selection"
+import { NodeGlyphShape } from "../NodeGlyphInterface"
+import { EdgeGlyphShape } from "../EdgeGlyphInterface"
 export class FlubberEdgeShape extends EdgeShape {
-	private readonly _lib: DGLOsSVGBaseClass;
 	constructor(lib: DGLOsSVGBaseClass) {
-		this._lib = lib
+		super(lib);
 	}
 	protected transformTo(sourceSelection: Selection<any, {}, any, {}>, shape: NodeGlyphShape | EdgeGlyphShape, targetSelection: Selection<any, {}, any, {}>) {
-		sourceSelection.transition().style("display", "none");
-		targetSelection.transition().style("display", null);
-	}
-	public get lib(): DGLOsSVGBaseClass {
-		return this._lib;
+		super.transformTo(sourceSelection, shape, targetSelection);
 	}
 } 
