40a9570e28e77dbacfa4957aa50040b34dae911f
Will Price <willprice22@hotmail.com>
2017-06-13 17:02:06 -0500

    Fixed all new node implementatnions

--- src/ts/lib/shapes/FlubberNodeShape.ts
+++ src/ts/lib/shapes/FlubberNodeShape.ts
@@ -1,15 +1,13 @@
 import { NodeShape } from "./NodeShape"
+import { DGLOsSVGBaseClass } from "../DGLOsSVGBaseClass"
+import { Selection } from "d3-selection"
+import { NodeGlyphShape } from "../NodeGlyphInterface"
+import { EdgeGlyphShape } from "../EdgeGlyphInterface"
 export class FlubberNodeShape extends NodeShape {
-	private readonly _lib: DGLOsSVGBaseClass;
 	constructor(lib: DGLOsSVGBaseClass) {
-		this._lib = lib
+		super(lib);
 	}
-
 	protected transformTo(sourceSelection: Selection<any, {}, any, {}>, shape: NodeGlyphShape | EdgeGlyphShape, targetSelection: Selection<any, {}, any, {}>) {
-		sourceSelection.transition().style("display", "none");
-		targetSelection.transition().style("display", null);
-	}
-	public get lib(): DGLOsSVGBaseClass {
-		return this._lib;
+		super.transformTo(sourceSelection, shape, targetSelection);
 	}
 }
--- src/ts/lib/shapes/NodeShape.ts
+++ src/ts/lib/shapes/NodeShape.ts
@@ -1,3 +1,17 @@
+import { DGLOsSVGBaseClass } from "../DGLOsSVGBaseClass"
+import { Selection } from "d3-selection"
+import { NodeGlyphShape } from "../NodeGlyphInterface"
+import { EdgeGlyphShape } from "../EdgeGlyphInterface"
 export class NodeShape {
-
+	private readonly _lib: DGLOsSVGBaseClass;
+	constructor(lib: DGLOsSVGBaseClass) {
+		this._lib = lib
+	}
+	get lib(): DGLOsSVGBaseClass {
+		return this._lib
+	}
+	protected transformTo(sourceSelection: Selection<any, {}, any, {}>, shape: NodeGlyphShape | EdgeGlyphShape, targetSelection: Selection<any, {}, any, {}>) {
+		sourceSelection.transition().style("display", "none");
+		targetSelection.transition().style("display", null);
+	}
 }
