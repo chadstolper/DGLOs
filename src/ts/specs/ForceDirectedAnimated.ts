51bffbbef37102875e7cb458715055dd808c552d
Will Price <willprice22@hotmail.com>
2017-06-16 17:26:08 -0500

    ForceDirectedAnimated node shape is now circles

--- src/ts/specs/ForceDirectedAnimated.ts
+++ src/ts/specs/ForceDirectedAnimated.ts
@@ -5,7 +5,7 @@ export class ForceDirectedAnimated extends Technique {
 	public draw() {
 		this.lib.drawEdgeGlyphs();
 		this.lib.drawNodeGlyphs();
-		this.lib.transformNodeGlyphsTo(this.lib.labelShape);
+		this.lib.transformNodeGlyphsTo(this.lib.circleShape);
 		this.lib.transformEdgeGlyphsTo(this.lib.sourceTargetLineShape);
 		this.lib.setNodeGlyphAttrs(new SVGAttrOpts("id", "grey", 10, 2, null, null));
 		this.lib.setEdgeGlyphAttrs(new SVGAttrOpts(null, "black", null, "1"));
