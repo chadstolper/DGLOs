007cf9eeb5662e0c477a54f41700eb95610773e5
Will Price <willprice22@hotmail.com>
2017-06-26 12:49:59 -0500

    trying to get FDA to work again

--- src/ts/main/willTest.ts
+++ src/ts/main/willTest.ts
@@ -23,7 +23,7 @@ json("data/radoslaw/emails.json", function (response: any) {
 	let g: DynamicGraph = new DynamicRadoslawGraph(response);
 	let svg: Selection<any, {}, any, {}> = select("body")
 	let lib: DGLOs = new DGLOsSVG(g, svg, width, height);
-	let vis: Technique = new MatrixTimeline(lib, {});
+	let vis: Technique = new ForceDirectedAnimated(lib, {});
 	vis.draw();
 })
 
