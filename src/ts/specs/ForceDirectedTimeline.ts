0156bd6cf4fc67a9283716566d9130ff86f7b0d4
Will Price <willprice22@hotmail.com>
2017-06-27 15:15:46 -0500

    FDT works for dummy, newcomb, and radoslaw as long as node shapes are circles

--- src/ts/specs/ForceDirectedTimeline.ts
+++ src/ts/specs/ForceDirectedTimeline.ts
@@ -9,7 +9,7 @@ export class ForceDirectedTimeline extends Technique {
 		this.lib.setNodeGlyphAttrs(new SVGAttrOpts("id", "grey", 2, 0, 10, null, null, 100, "12pt"));
 		this.lib.setEdgeGlyphAttrs(new SVGAttrOpts(null, "black", 1));
 		this.lib.transformEdgeGlyphsTo(this.lib.sourceTargetLineShape);
-		this.lib.transformNodeGlyphsTo(this.lib.labelShape);
+		this.lib.transformNodeGlyphsTo(this.lib.circleShape);
 		// this.lib.enableEnterExitColoring();
 		this.lib.positionNodesAndEdgesForceDirected(true);
 	}
