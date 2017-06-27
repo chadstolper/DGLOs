e572ed528853f38530129d8c28840fb4664572f3
Will Price <willprice22@hotmail.com>
2017-06-27 15:14:06 -0500

    Matrix Timeline works for newcomb, dummy, and radoslaw

--- src/ts/specs/MatrixTimeline.ts
+++ src/ts/specs/MatrixTimeline.ts
@@ -3,11 +3,13 @@ import { SVGAttrOpts } from "../lib/DGLOsSVG";
 
 export class MatrixTimeline extends Technique {
 	public draw(): void {
+		let attr = new SVGAttrOpts("id", "black", 1, 1, 10, 2000, 2000);
+		let attr2 = new SVGAttrOpts("blue", "black", 1, 1, 10, 2000, 2000);
 		this.lib.drawNodeGlyphs();
 		this.lib.drawEdgeGlyphs();
 		this.lib.drawTimesteps();
-		this.lib.setNodeGlyphAttrs(new SVGAttrOpts("black", "black", 0));
-		this.lib.setEdgeGlyphAttrs(new SVGAttrOpts("blue", "black", 1, 0, null, 1500 / (this._library.data.timesteps[0].nodes.length - 1), 1500 / (this._library.data.timesteps[0].nodes.length - 1), null));
+		this.lib.setNodeGlyphAttrs(attr);
+		this.lib.setEdgeGlyphAttrs(attr2);
 		this.lib.transformEdgeGlyphsTo(this.lib.rectShape);
 		this.lib.transformNodeGlyphsTo(this.lib.labelShape);
 		this.lib.positionNodeGlyphsMatrix();
