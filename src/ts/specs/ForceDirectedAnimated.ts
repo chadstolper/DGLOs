455ce55a07ac4a7c6e18a2433895a585381f3d86
Sanfordm <sanfordm@southwestern.edu>
2017-06-27 10:33:39 -0500

    im so confused

--- src/ts/specs/ForceDirectedAnimated.ts
+++ src/ts/specs/ForceDirectedAnimated.ts
@@ -1,5 +1,5 @@
 import { Technique } from "./Technique";
-import { SVGAttrOpts } from "../lib/DGLOsSVG";
+import { SVGAttrOpts, SimulationAttrOpts } from "../lib/DGLOsSVG";
 
 export class ForceDirectedAnimated extends Technique {
 	public draw() {
@@ -9,6 +9,7 @@ export class ForceDirectedAnimated extends Technique {
 		this.lib.transformEdgeGlyphsTo(this.lib.sourceTargetLineShape);
 		this.lib.setNodeGlyphAttrs(new SVGAttrOpts("id", "grey", 2, 0, 10, null, null, 100, "16pt"));
 		this.lib.setEdgeGlyphAttrs(new SVGAttrOpts(null, "black", 1, 0));
+		this.lib.setSimulationAttrs(new SimulationAttrOpts(true, true))
 		this.lib.positionNodesAndEdgesForceDirected(true);
 		this.lib.enableStepping();
 	}
