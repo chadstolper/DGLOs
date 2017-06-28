f4b2888c740e1a5f69410af997efd36f4355de48
Will Price <willprice22@hotmail.com>
2017-06-28 16:37:49 -0500

    Fixed a set of errors occuring with simulation calculation and gestalt Glyphs

--- src/ts/lib/DGLOsMatt.ts
+++ src/ts/lib/DGLOsMatt.ts
@@ -211,7 +211,11 @@ export class DGLOsMatt extends DGLOsSVGCombined {
 			//update edges in map; run update of simulation on all edges at the current timestep
 			this._edgeGlyphMap.forEach(function (GlyphMap: Map<EdgeGlyphShape, Selection<any, {}, any, {}>>, SVGPosition: number) {
 				GlyphMap.forEach(function (glyphs: Selection<any, {}, any, {}>, shape: EdgeGlyphShape) {
-					shape.draw(glyphs, self.dataToDraw, self.timeStampIndex, self._attrOpts, self.width, self.height, self.enterExitColorEnabled);
+					if (shape.shapeType === "Gestalt" && self.currentEdgeShape.shapeType != "Gestalt") {
+
+					} else {
+						shape.draw(glyphs, self.dataToDraw, self.timeStampIndex, self._attrOpts, self.width, self.height, self.enterExitColorEnabled);
+					}
 				});
 			});
 			//update nodes in map; run update of simulation on all NodeGlyphs at the current timestep
@@ -231,7 +235,11 @@ export class DGLOsMatt extends DGLOsSVGCombined {
 			//update edges in map; run update of simulation on all edges accross multiple SVG elements
 			this._edgeGlyphMap.forEach(function (GlyphMap: Map<EdgeGlyphShape, Selection<any, {}, any, {}>>, SVGPosition: number) {
 				GlyphMap.forEach(function (glyphs: Selection<any, {}, any, {}>, shape: EdgeGlyphShape) {
-					shape.draw(glyphs, self.dataToDraw, SVGPosition, self._attrOpts, self.width, self.height, self.enterExitColorEnabled);
+					if (shape.shapeType === "Gestalt" && self.currentEdgeShape.shapeType != "Gestalt") {
+
+					} else {
+						shape.draw(glyphs, self.dataToDraw, SVGPosition, self._attrOpts, self.width, self.height, self.enterExitColorEnabled);
+					}
 				});
 			});
 			//update nodes in map; run update of simulation on all NodeGlyphs accross multiple SVG elements
