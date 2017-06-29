643a3d1e0d63c9fb3bbecfda95c80f4451f494da
Will Price <willprice22@hotmail.com>
2017-06-29 10:03:02 -0500

    updated flubber

--- src/ts/main/willTest.ts
+++ src/ts/main/willTest.ts
@@ -22,8 +22,8 @@ import { DynamicFiveKingsGraph } from "../data/FiveKingsGraph"
 //json("data/dummy/dummy2.json", function (response: any) {
 //json("data/dummy/dummy.json", function (response: any) {
 //json("data/radoslaw/emails.json", function (response: any) {
-//json("data/newcomb/newcomb.json", function (response: any) {
-json("data/fivekings/fivekings.json", function (response: any) {
+json("data/newcomb/newcomb.json", function (response: any) {
+	//json("data/fivekings/fivekings.json", function (response: any) {
 	let width: number, height: number;
 	//width = height = 1000;
 	width = height = 2000;
@@ -31,18 +31,18 @@ json("data/fivekings/fivekings.json", function (response: any) {
 	//width = 1000;
 	//let g: DynamicGraph = new DynamicDrinkGraph(response);
 	//let g: DynamicGraph = new DynamicRadoslawGraph(response);
-	let g: DynamicGraph = new DynamicFiveKingsGraph(response);
+	//let g: DynamicGraph = new DynamicFiveKingsGraph(response);
 	//let g: DynamicGraph = new DynamicNewcombGraph(response);
-	//let g: DynamicGraph = new DynamicNewcombTopFiveGraph(response);
+	let g: DynamicGraph = new DynamicNewcombTopFiveGraph(response);
 
 
 	let svg: Selection<any, {}, any, {}> = select("body")
 	let lib: DGLOs = new DGLOsSVG(g, svg, width, height);
 	//let vis: Technique = new Egograph(lib, {}); // WORKS FOR DUMMY, RADOSLAW, AND NEWCOMB
 	//let vis: Technique = new ForceDirectedAnimated(lib, {}); // WORKS FOR DUMMY, RADOSLAW
-	let vis: Technique = new ForceDirectedTimeline(lib, {}); // WORKS FOR DUMMY, RADOSLAW, AND NEWCOMB
+	//let vis: Technique = new ForceDirectedTimeline(lib, {}); // WORKS FOR DUMMY, RADOSLAW, AND NEWCOMB
 	//let vis: Technique = new GestaltGlyphs(lib, {});
-	//let vis: Technique = new MatrixAnimated(lib, {}); // WORKS FOR DUMMY, RADOSLAW, AND NEWCOMB
+	let vis: Technique = new MatrixAnimated(lib, {}); // WORKS FOR DUMMY, RADOSLAW, AND NEWCOMB
 	//let vis: Technique = new MatrixTimeline(lib, {}); // WORKS FOR DUMMY, RADOSLAW, AND NEWCOMB
 	//let vis: Technique = new GMap(lib, {}); // TODO: VORONOI LOSES DATA SOMETIMES
 	vis.draw();
