4c198347b6c098c68e3f1d79f3b97691aed6ef33
Will Price <willprice22@hotmail.com>
2017-07-07 20:16:01 -0500

    Cleaned up MiserablesGraph for the linter, changed `let` to `const` where appropriate, no more compile errors in MiserablesGraph.ts

--- src/ts/data/MiserablesGraph.ts
+++ src/ts/data/MiserablesGraph.ts
@@ -14,25 +14,25 @@ export class Cooccurence extends Edge {
 
 export class LesMiserablesGraph extends Graph {
 	public constructor(rawNodeData: Array<any>, rawEdgeData: Array<any>, timestamp: number) {
-		let nodeData = new Array<Node>();
-		let edgeData = new Array<Edge>();
-		let index = 0;
-		for (let n of rawNodeData) {
-			let node = new Person(n.id, index, n.group, timestamp);
+		const nodeData: Array<Node> = new Array<Node>();
+		const edgeData: Array<Edge> = new Array<Edge>();
+		let index: number = 0;
+		for (const n of rawNodeData) {
+			const node: Person = new Person(n.id, index, n.group, timestamp);
 			nodeData.push(node);
 			index++;
 		}
-		for (let e of rawEdgeData) {
-			let source: Node = nodeData.find(function (n: Node): boolean {
+		for (const e of rawEdgeData) {
+			const source: Node = nodeData.find(function (n: Node): boolean {
 				return n.id === e.source;
 			});
 
-			let target: Node = nodeData.find(function (n: Node): boolean {
+			const target: Node = nodeData.find(function (n: Node): boolean {
 				return n.id === e.target;
 			});
-			let id: string = "" + source.id + ":" + target.id;
+			const id: string = "" + source.id + ":" + target.id;
 
-			let edge = new Cooccurence(id, source, target, e.value, timestamp);
+			const edge: Cooccurence = new Cooccurence(id, source, target, e.value, timestamp);
 			edgeData.push(edge);
 		}
 		nodeData.sort(function (a: Node, b: Node): number {
@@ -44,13 +44,11 @@ export class LesMiserablesGraph extends Graph {
 
 export class DynamicLesMiserablesGraph extends DynamicGraph {
 	public constructor(response: any) {
-		// console.log(response);
-		let graphs: Array<LesMiserablesGraph> = new Array<LesMiserablesGraph>();
-
-		let rawNodeData: Array<any> = response.nodes;
-		let rawEdgeData: Array<any> = response.links;
-		let timestep: number = response.timestep;
-		let g: LesMiserablesGraph = new LesMiserablesGraph(rawNodeData, rawEdgeData, timestep);
+		const graphs: Array<LesMiserablesGraph> = new Array<LesMiserablesGraph>();
+		const rawNodeData: Array<any> = response.nodes;
+		const rawEdgeData: Array<any> = response.links;
+		const timestep: number = response.timestep;
+		const g: LesMiserablesGraph = new LesMiserablesGraph(rawNodeData, rawEdgeData, timestep);
 		graphs.push(g);
 
 		super(graphs);
